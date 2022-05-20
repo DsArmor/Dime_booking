bcf189241231e458f8cab82019a257908993f4f5
ArmorDs <zeleznoff84-00@mail.ru>
2022-05-20 20:04:36 +0300

    vaadin base config and login page

--- src/main/java/com/dancompany/booking/security/ApplicationSecurityConfig.java
+++ src/main/java/com/dancompany/booking/security/ApplicationSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -23,6 +24,38 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     private final PasswordEncoder passwordEncoder;
     private final UserDetailsService userDetailsService;
 
+    private static final String LOGIN_PROCESSING_URL = "/login";
+    private static final String LOGIN_FAILURE_URL = "/login?error";
+    private static final String LOGIN_URL = "/login";
+    private static final String LOGOUT_SUCCESS_URL = "/login";
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        // Vaadin handles CSRF internally
+//        http.csrf().disable()
+//
+//                // Register our CustomRequestCache, which saves unauthorized access attempts, so the user is redirected after login.
+//                .requestCache().requestCache(new CustomRequestCache())
+//
+//                // Restrict access to our application.
+//                .and().authorizeRequests()
+//
+//                // Allow all Vaadin internal requests.
+//                .requestMatchers(SecurityUtils::isFrameworkInternalRequest).permitAll()
+//
+//                // Allow all requests by logged-in users.
+//                .anyRequest().authenticated()
+//
+//                // Configure the login page.
+//                .and().formLogin()
+//                .loginPage(LOGIN_URL).permitAll()
+//                .loginProcessingUrl(LOGIN_PROCESSING_URL)
+//                .failureUrl(LOGIN_FAILURE_URL)
+//
+//                // Configure logout
+//                .and().logout().logoutSuccessUrl(LOGOUT_SUCCESS_URL);
+//    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
@@ -33,10 +66,10 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                     .antMatchers("/backpacker/{backpackerId}/**")
                         .access("@userSecurity.hasUserId(authentication, #backpackerId) and hasAuthority('USER')")
                     .anyRequest().authenticated()
-                .and()
-                    .httpBasic()
-                .and()
-                    .formLogin().permitAll()
+                .and().formLogin()
+                    .loginPage(LOGIN_URL).permitAll()
+                    .loginProcessingUrl(LOGIN_PROCESSING_URL)
+                    .failureUrl(LOGIN_FAILURE_URL)
                 .and()
                     .logout().permitAll();
     }
@@ -54,4 +87,29 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         return daoAuthenticationProvider;
     }
 
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers(
+                // Client-side JS
+                "/VAADIN/**",
+
+                // the standard favicon URI
+                "/favicon.ico",
+
+                // the robots exclusion standard
+                "/robots.txt",
+
+                // web application manifest
+                "/manifest.webmanifest",
+                "/sw.js",
+                "/offline.html",
+
+                // icons and images
+                "/icons/**",
+                "/images/**",
+                "/styles/**",
+
+                // (development mode) H2 debugging console
+                "/h2-console/**");
+    }
 }
